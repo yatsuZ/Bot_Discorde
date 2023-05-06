f133d40081b28b68b8966fc4c55289af40251030
yatsuZ <y.zaoui.pro@gmail.com>
2023-05-06 22:17:39 +0200

    la commande aide ameliorer. faire ce que j'ai a faire.

--- code/Bot.py
+++ code/Bot.py
@@ -1,3 +1,4 @@
+###################################################################################### Import
 import nest_asyncio
 import asyncio
 import discord
@@ -9,6 +10,8 @@ from dotenv import load_dotenv
 from All_structure.Node_historique import NodeH, PileH
 from All_structure.Serveur import List_Serveur, Serveur_Discorde
 
+###################################################################################### Initial var
+
 All_Serveurs = List_Serveur()
 
 mon_id = 284082499649273856
@@ -18,7 +21,113 @@ nest_asyncio.apply()
 intents = discord.Intents.all()
 
 yassbot = commands.Bot(command_prefix="!", intents = intents)
-###
+
+###################################################################################### Texte + Comande Aide
+Explication_commande = \
+    "> **!aide**:\n>\t- Affiche le répertoire des commandes.\n\n"+\
+    "> **!markdown**:\n>\t- Affiche tous les effets de texte qu'on peut faire sur Discord (liste non exhaustive).\n"+\
+    "> **!wsh**:\n>\t- Le bot vous saluera en vous mentionnant et mentionnera son créateur.\n\n"+\
+    "> **!king_krimson**:\n>\t- Les 10 derniers messages seront supprimés et il affichera un gif de King Crimson.\n>\t(JOJO REF TU CONNAIS.)\n\n"+\
+    "> **!derniere_commande**:\n>\t- Affiche les informations suivantes : l'auteur, le contenu et la date de la dernière commande envoyée dans le serveur, ainsi que le nom du canal.\n"
+
+Explication_events = \
+    "> **on_message**:\n>\t- Quand quelqu'un envoie un message. ***L'ÉVÉNEMENT LE PLUS IMPORTANT.*** Cela permet de savoir si quelqu'un a envoyé une commande.\n>\tRéagit si quelqu'un envoie :\n"+\
+    "\n```python\nQui commence par : \"hello ...\".\nExactement : \"ping\", \"quoicoubaka\"\n```"+\
+    "> **on_message_delete**:\n>\t- Quand un message est supprimé. \"**Un message a été supprimé.**\" sera affiché dans le serveur de YassBot.\n"+\
+    "> **on_reaction_add**:\n>\t- Quand quelqu'un lâche une réaction.\n"+\
+    "\n```ansi\n[0;37m1. Si vous faites \"👍\" à l'un de vos propres messages, je le mentionnerai avec sarcasme -_-.\n" +\
+    "2. Permet d'interagir avec la commande `!aide`.\n```"
+
+A_Faire = "```ansi\n" + \
+    "- Ajouter des informations et des emojis à aide.\n" + \
+    "----\n" + \
+    "- Créer une commande historique qui prendra des paramètres pour historique global ou personnel, et qui pourra aussi supprimer un message de l'historique. Faire en sorte qu'une seule personne à la fois puisse y avoir accès.\n" + \
+    "----\n" + \
+    "- Faire une conversation d'arbre binaire (pas trop compris, attends Abdel pour le faire).\n" + \
+    "----\n" + \
+    "- Essayer de créer un système de devinette comme Akinator. Créer un Akinator par serveur. Les utilisateurs pourront le personnaliser au fur et à mesure.\n" + \
+    "----\n" + \
+    "- Faire la gestion de l'historique de serveur de liste avec des hashtable pour que ce soit plus rapide.\n" + \
+    "----\n" + \
+    "- Tout sauvegarder en JSON pour que tout soit sauvegardé.\n" + \
+    "----\n" + \
+    "- Rajouter des trucs.\n" + \
+    "----\n" + \
+    "- Faire une variable ON/OFF pour chaque événement.\n" + \
+    "----\n" + \
+    "- Essayer de créer un jeu Loup-Garou, où le bot sera le MJ. Mon bot créera un canal textuel et vocal pour les classes morts et vivants, et il prendra les choix de chaque joueur pour créer la partie.\n" + \
+    "----\n" + \
+    "- Faire un event chip si 2 ou plusieur utilisateur ecrivve exeactement le meme message a la minute pres alors bot enverra un message et le premier qui ecrirea \"chip\" mutera l'autre pendant 5min ou plus ou si quelqun mention celui qui sera mute.\n" + \
+    "```"
+timer = 30
+
+message_aide = "**Yo Yasbot va t'aider !!**\n" + \
+    "Salut, sur quoi as-tu besoin d'aide ? Sélectionne l'emoji correspondant.\n" + \
+    "\n-\t ℹ️ : Je me présente :```Si tu te demandes qui est Yassbot ?```" + \
+    "\n-\t 📄 : Je liste toutes mes commandes.```Si tu te demandes ce que je peux faire ?```" + \
+    "\n-\t 🪄 : Je liste tous mes événements.```Si tu te demandes quelles interactions je peux avoir ?```" + \
+    "\n-\t ⚒️ : Je liste ce que je pourrais faire.```Si tu te demandes ce qui est en construction ?```" + \
+    "\n-\t ❌ : Si tu n'as plus besoin d'aide,```(Le message ne se supprimera pas).```" + \
+    "\n\n***Seul l'auteur de ce message peut choisir et interagir avec ce message***.\n"+\
+    "||Apres ***"+str(timer)+"*** secondes ce message se GOGOGADGET-autodétruira :detective:.||"
+emojie_aide = ["ℹ️", "📄", "🪄", "⚒️", "❌"]
+
+async def affichage_aide(reaction:discord.reaction.Reaction, id_autheur, message:discord.message.Message):
+    if reaction.emoji == "❌":
+        await reaction.message.channel.send("La ❌ a etais selectioner donc")
+        return
+    try:
+        msg = await message.channel.fetch_message(message.id)
+    except discord.NotFound:
+        return
+    supr = True
+    for reac in reaction.message.reactions:
+        if reac.emoji == "❌":
+             async for user in reaction.users():
+                 if user.id == id_autheur:
+                     supr = False
+                     break
+    if (supr):
+        await reaction.message.delete()
+    if (reaction.emoji == "📄"):
+        await reaction.message.channel.send("Voici toute les commande d'ont je dispose :\n\n"+Explication_commande)
+    elif (reaction.emoji == "ℹ️"):
+        Presentation_Yassbot: str = "Salut, je suis " + str(yassbot.user.mention) + " créé par Yassine / yatsu.\nJe suis créé à des fins pédagogiques et j'espère que Yassine m'améliorera dans le futur et ne m'oubliera pas ;-;.\nSi tu souhaites voir l'avancement du projet ou des idées de choses que je peux faire, je te conseille de jeter un coup d'œil ici ;) : https://github.com/yatsuZ/Bot_Discorde"
+        await reaction.message.channel.send(Presentation_Yassbot)
+    elif reaction.emoji == "🪄":
+        await reaction.message.channel.send("Voici tous les ÉVÉNEMENTS qui sont actifs sur le bot :\n\n" + "\n".join(Explication_events))
+    elif reaction.emoji == "⚒️":
+        await reaction.message.channel.send("Voici tout ce qu'il me reste à faire :\n\n" + A_Faire)
+
+
+@yassbot.command(name="aide")
+@commands.guild_only()
+async def aide(ctx):
+    '''
+    Affiche un message d'aide sur le serveur.
+    '''
+    messgae_auteur: discord.Message = All_Serveurs.get_last_data(ctx.guild.id)
+    auteur_id = messgae_auteur.author.id
+    message: discord.Message = await ctx.send(message_aide)
+    for emojie in emojie_aide:
+        await message.add_reaction(emojie)
+    await asyncio.sleep(timer)
+    try:
+        msg = await ctx.channel.fetch_message(message.id)
+    except discord.NotFound:
+        return
+    for reaction in msg.reactions:
+        if reaction.emoji == "❌":
+            async for user in reaction.users():
+                if user.id == auteur_id:
+                    await ctx.send("Le message d'aide ne sera pas suprimer.")
+                    return
+            break
+    await message.delete()
+
+
+###################################################################################### texte Markdown + Commande Markdown
+
 Makrdown = (
     "Voici un message de démonstration avec différentes utilisations de Markdown :"
     "\n**Texte en gras**"
@@ -52,6 +161,8 @@ async def show_all_markdown_discorde(ctx):
     await ctx.send("test")
     await ctx.send(Makrdown)
 
+###################################################################################### Commande wsh
+
 @yassbot.command(name="wsh")
 @commands.guild_only()
 async def say_hello(ctx):
@@ -63,6 +174,8 @@ async def say_hello(ctx):
     yatsu = yassbot.get_user(mon_id) # Obtenir un objet utilisateur en utilisant son ID (c moi)
     await ctx.send("Wsh "+user.mention+".\nJe suis un yassbot pas encore fini fait par "+ yatsu.mention +".")
 
+###################################################################################### Commande King Krimson
+
 @yassbot.command(name="king_krimson")
 async def delete(ctx):
     '''
@@ -72,26 +185,24 @@ async def delete(ctx):
     '''
     async for message in ctx.channel.history(limit=10):
         await message.delete()
+        await asyncio.sleep(0.5)
+        
     if (bool(random.getrandbits(1))):
         gif_king_krimson = "https://media.tenor.com/lNzPjBeAeB8AAAAd/king-crimson-cartoon.gif"
     else:
         gif_king_krimson = "https://media.tenor.com/vUdvSJpdQq8AAAAd/king-crimson.gif"
-    await ctx.send("Lorsque le Stand arrête d'effacer le temps, les victimes ne se souviennent plus de ce qui s'est passé.\n")
+    await ctx.send("`Lorsque le Stand arrête d'effacer le temps, les victimes ne se souviennent plus de ce qui s'est passé.`\n")
     await ctx.send(gif_king_krimson)
+
+###################################################################################### Event Suppression de message
 
 @yassbot.event
 async def on_message_delete(message):
-    print("Un message a été supprimé.")
+    pass
+    # print("Un message a été supprimé.")
     # print(f"Contenu du message : {message.content}")
 
-
-Explication_commande = [
-    "> **!aide**:\n>\t- Affiche le repertoire des commandes.\n\n",
-    "> **!wsh**:\n>\t- Le bot vous salura en vous mentionant et mentionara son createur.\n\n",
-    "> **!king_krimson**:\n>\t- Les 10 derniers message serons suprimer + doit afficher un gif de king_krimson.\n>\t(JOJO REF TU CONNAIS.)\n\n",
-    "> **!markdown**:\n>\t- Affiche tout les effets de texte qu'on peut faire sur discorde liste non exhaustife.\n",
-    "> **!derniere_commande**:\n>\t- Affiche dans quelle serveur, chanelle, le contenue du la commande par qui quand et ptetre d'autre informations.\n"
-                        ]
+###################################################################################### Event Reaction
 
 @yassbot.event
 async def on_reaction_add(reaction:discord.reaction.Reaction, user:discord.member.Member):
@@ -106,58 +217,7 @@ async def on_reaction_add(reaction:discord.reaction.Reaction, user:discord.membe
         if user.id == auteur_id.author.id and reaction.message.content == message_aide and reaction.message.author.id == yassbot.user.id:
             await affichage_aide(reaction, auteur_id.author.id, reaction.message)
 
-async def affichage_aide(reaction:discord.reaction.Reaction, id_autheur, message:discord.message.Message):
-    if reaction.emoji != "📄" and reaction.emoji != "ℹ️" or reaction.emoji == "❌":
-        await reaction.message.channel.send("Je ne suprime rien.")
-        return
-    try:
-        msg = await message.channel.fetch_message(message.id)
-    except discord.NotFound:
-        return
-    supr = True
-    for reac in reaction.message.reactions:
-        if reac.emoji == "❌":
-             async for user in reaction.users():
-                 if user.id == id_autheur:
-                     supr = False
-                     break
-    if (supr):
-        await reaction.message.delete()
-    if (reaction.emoji == "📄"):
-        await reaction.message.channel.send("Voici toute les commande d'ont je dispose :\n\n")
-        for text in Explication_commande:
-            await reaction.message.channel.send(text)
-    elif (reaction.emoji == "ℹ️"):
-        Presentation_Yassbot: str = str("Salut Je suis " + str(yassbot.user.mention) + " crée pas Yassine / yatsu.\nJe suis crée à des fin pédagogique et jespere que yassine m'ameliora dans le futur et ne moubliera pas ;-;.\nSi tu shouaite voir l'avancement du projet ou des idée de chose que je peux faire je te conseil de jeter un coup d'oeil à sa ;) : https://github.com/yatsuZ/Bot_Discorde")
-        await reaction.message.channel.send(Presentation_Yassbot)
-
-message_aide = "**Yo Yasbot va t'aidé!!**\nSalut sur quoi a tu besoin d'aide ? \`Selection l'emojie.\` \n-\t 📄 : Je liste toute mes commande.\n-\t ℹ️: Je me presente\n-\t ❌: Si tu n'as plus besoin d'aide, (Le message ne se surpimera pas).\n\nSeul l'auteur de ce message peux choisir. Si l'auteur du message ne choisi rien apres plus de 20 seconde\nou apres avoir choisi ce message s'auto detruira"
-
-@yassbot.command(name="aide")
-@commands.guild_only()
-async def aide(ctx):
-    '''
-    Affiche un message d'aide sur le serveur.
-    '''
-    messgae_auteur: discord.Message = All_Serveurs.get_last_data(ctx.guild.id)
-    auteur_id = messgae_auteur.author.id
-    message: discord.Message = await ctx.send(message_aide)
-    await message.add_reaction("📄")
-    await message.add_reaction("ℹ️")
-    await message.add_reaction("❌")
-    await asyncio.sleep(3)
-    try:
-        msg = await ctx.channel.fetch_message(message.id)
-    except discord.NotFound:
-        return
-    for reaction in msg.reactions:
-        if reaction.emoji == "❌":
-            async for user in reaction.users():
-                if user.id == auteur_id:
-                    await ctx.send("La ❌ a etais selectioner donc le message ne sera pas supprimer.")
-                    return
-            break
-    await message.delete()
+###################################################################################### Commande derniere _commande
 
 @yassbot.command(name="derniere_commande")
 @commands.guild_only()
@@ -188,6 +248,7 @@ async def derniere_commande(ctx):
         "```"
     await ctx.send(message)
 
+###################################################################################### EVENT Message
 
 @yassbot.event
 async def on_message(message):
@@ -210,15 +271,17 @@ async def on_message(message):
         await message.channel.send("pong")
     if message.content == "quoicoubaka":
         await message.channel.send("https://media.tenor.com/GOYRQva4UeoAAAAd/anime-dance.gif")
-
-
+    if (message.content == None or message.content == ""):
+        return
     if message.content[0] == "!":
         command_name = message.content[1:].split()[0]
         command = yassbot.get_command(command_name)
         if command:
             serveur.historique.push(message)
             await yassbot.process_commands(message)
-###
+
+###################################################################################### ACTVIER LE BOT
+
 load_dotenv()
 TOKENBOT = os.getenv('tokenBot')  # récupérer la valeur de votre token
 if (TOKENBOT):
